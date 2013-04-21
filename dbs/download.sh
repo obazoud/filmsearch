79db9b9ce218f016c1210f49a66ded5cf49baee5
James Addison <jay@jp-hosting.net>
2013-04-21 23:10:03 +0100

    Brief download script

---
+++ dbs/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to download the full, initial imdb files for import
+BASE="ftp://ftp.sunet.se/pub/tv+movies/imdb/"
+FILES=( "actors" "actresses" "cinematographers" "composers" "countries" "directors" "distributors" "editors" "genres" "keywords" "language" "locations" "movies" "producers" "release-dates" "running-times" "soundtracks" "writers" )
+
+for file in ${FILES}
+do
+	wget "${BASE}/${file}.list.gz"
+done;
