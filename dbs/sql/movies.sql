7a7a1edfc31bc3d39460c70ed40d437232dfcf03
James Addison <jay@jp-hosting.net>
2013-04-24 07:12:43 +0100

    Move left join aggregates into subquery aggregates

--- dbs/sql/movies.sql
+++ dbs/sql/movies.sql
@@ -4,19 +4,37 @@ select
     t.production_year,
     ln.info as language,
     cn.info as country,
-    array_agg(distinct(dr_n.name)) as directors,
-    array_agg(distinct(coalesce(am_n.name, af_n.name))) as cast,
-    array_agg(distinct(lc.info)) as locations
+    (
+        select array_agg(n.name)
+        from cast_info as dr
+        join name as n on n.id = dr.person_id
+        where dr.movie_id = t.id
+        and dr.role_id = 8
+    ) as directors,
+    (
+        select array_agg(n.name)
+        from cast_info as ac
+        join name as n on n.id = ac.person_id
+        where ac.movie_id = t.id
+        and ac.role_id in (1, 2)
+        and ac.nr_order <= 10
+    ) as cast,
+    (
+        select array_agg(lc.info)
+        from movie_info as lc
+        where lc.movie_id = t.id
+        and lc.info_type_id = 18
+        and lc.note is null
+    ) as locations,
+    (
+        select array_agg(k.keyword)
+        from  movie_keyword as mk
+        join keyword as k on k.id = mk.id
+        where mk.movie_id = t.id
+    ) as keywords
 from title as t
 left join movie_info as ln on ln.movie_id = t.id and ln.info_type_id = 4
 left join movie_info as cn on cn.movie_id = t.id and cn.info_type_id = 8
-left join movie_info as lc on lc.movie_id = t.id and lc.info_type_id = 18 and lc.note is null
-join cast_info as dr on dr.movie_id = t.id and dr.role_id = 8
-left join cast_info as am on am.movie_id = t.id and am.role_id = 1 and am.nr_order <= 10
-left join cast_info as af on af.movie_id = t.id and af.role_id = 2 and af.nr_order <= 10
-join name as dr_n on dr_n.id = dr.person_id
-left join name as am_n on am_n.id = am.person_id
-left join name as af_n on af_n.id = af.person_id
 where t.kind_id = 1
 and t.title = 'Brazil'
 and t.production_year = 1985
